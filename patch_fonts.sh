c68d44496761e91f0f1ab261dab968c973c3140b
Ngo Tuan Dzung <zun1903@gmail.com>
2025-03-02 20:27:29 +0700

    chore: further cleaning up build dir

--- patch_fonts.sh
+++ patch_fonts.sh
@@ -5,21 +5,20 @@ WORKING_DIR=Iosevka/dist/Tepiosevka/TTF
 
 mkdir -p TepiosevkaNerd
 for font in `find $WORKING_DIR -type f -name "*.otf" -o -name "*.ttf" -o -name "*.woff" -o -name "*.eot" -o -name "*.ttc"`; do
-  fontforge -script nerd/font-patcher \
-    -q \
-    -s \
-    -l \
-    -c \
+  fontforge -script nerd/font-patcher $font \
     --careful \
-    -out ./TepiosevkaNerdMono \
-    $font >> nerd.log 2>&1
+    --complete \
+    --mono \
+    --quiet \
+    --no-progressbars \
+    --outputdir build/TepiosevkaNerdMono
 
-  fontforge -script nerd/font-patcher \
-    -q \
-    -l \
-    -c \
+  fontforge -script nerd/font-patcher $font \
     --careful \
-    --makegroups -1 \
-    -out ./TepiosevkaNerd \
-    $font >> nerd.log 2>&1
+    --complete \
+    --quiet \
+    --no-progressbars \
+    --outputdir build/TepiosevkaNerd
 done
+
+mv $WORKING_DIR/Tepiosevka build/
