fa006fa6479fec2b46c008b8dbfab68897570bb4
Ngo Tuan Dzung <zun1903@gmail.com>
2025-03-02 14:03:18 +0700

    feat: add cleanup steps

--- patch_fonts.sh
+++ patch_fonts.sh
@@ -6,7 +6,12 @@ WORKING_DIR=Iosevka/dist/Tepiosevka/TTF
 mkdir -p TepiosevkaNerd
 for font in `find $WORKING_DIR -type f -name "*.otf" -o -name "*.ttf" -o -name "*.woff" -o -name "*.eot" -o -name "*.ttc"`; do
   fontforge -script nerd/font-patcher \
+    -q \
+    -s \
+    -l \
+    -c \
+    --careful \
+    --makegroups -1 \
     -out ./TepiosevkaNerd \
-    -c -q -l -s --careful --makegroups -1 \
-    $font > /dev/null
+    $font >> nerd.log 2>&1
 done
