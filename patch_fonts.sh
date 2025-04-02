d24a5b5c160a12aa09bc9a230ec9537955101247
Ngo Tuan Dzung <zun1903@gmail.com>
2025-04-02 21:03:11 +0700

    refactor: build non mono

--- patch_fonts.sh
+++ patch_fonts.sh
@@ -3,9 +3,8 @@ set -Eeuo pipefail
 
 WORKING_DIR=Iosevka/dist/Tepiosevka/TTF
 
-mkdir -p TepiosevkaNerd
-for font in `find $WORKING_DIR -type f -name "*.otf" -o -name "*.ttf" -o -name "*.woff" -o -name "*.eot" -o -name "*.ttc"`; do
-  fontforge -script nerd/font-patcher $font \
+for font in $(find $WORKING_DIR -type f -name "*.otf" -o -name "*.ttf" -o -name "*.woff" -o -name "*.eot" -o -name "*.ttc"); do
+  fontforge -script nerd/font-patcher "$font" \
     --careful \
     --complete \
     --mono \
@@ -13,4 +12,10 @@ for font in `find $WORKING_DIR -type f -name "*.otf" -o -name "*.ttf" -o -name "
     --no-progressbars \
     --makegroups -1 \
     --outputdir build/Tepiosevka
+
+  fontforge -script nerd/font-patcher "$font" \
+    --complete \
+    --quiet \
+    --no-progressbars \
+    --outputdir build/TepiosevkaNF
 done
